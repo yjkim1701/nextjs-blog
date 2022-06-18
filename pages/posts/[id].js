560845d52c514f5a9ba91f4904529a60b4cb568b
Young Jun Kim <yjkim10@hist.co.kr>
2022-06-18 15:23:39 +0900

    example complete

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import { Head } from "next/head";
+
+import Date from "../../components/date";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <article>
+                <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+            </article>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        }
+    }
+}
